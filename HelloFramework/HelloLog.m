42ba96daba61e0e980a85e552cd88af068662379
Ted Lin <lintide@gmail.com>
2024-07-11 16:22:17 +0800

    Hello Framework

---
+++ HelloFramework/HelloLog.m
@@ -0,0 +1,15 @@
+//
+//  HelloLog.m
+//  HelloFramework
+//
+//  Created by Ted Lin on 2024/7/10.
+//
+
+#import "HelloLog.h"
+
+@implementation HelloLog
+
++ (void)log {
+    NSLog(@"hello log");
+}
+@end
