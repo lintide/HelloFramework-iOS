f65c37272ff5e269676386456a28583f1e7b9413
Ted Lin <lintide@gmail.com>
2024-07-13 11:15:00 +0800

    生成 xcframework

---
+++ Products/HelloFramework.xcframework/ios-arm64/HelloFramework.framework/Headers/HelloLog.h
@@ -0,0 +1,18 @@
+//
+//  HelloLog.h
+//  HelloFramework
+//
+//  Created by Ted Lin on 2024/7/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HelloLog : NSObject
+
++ (void)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Products/HelloFramework.xcframework/ios-arm64_x86_64-simulator/HelloFramework.framework/Headers/HelloFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelloFramework.h
+//  HelloFramework
+//
+//  Created by Ted Lin on 2024/7/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloFramework.
+FOUNDATION_EXPORT double HelloFrameworkVersionNumber;
+
+//! Project version string for HelloFramework.
+FOUNDATION_EXPORT const unsigned char HelloFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloFramework/PublicHeader.h>
+
+#import <HelloFramework/HelloLog.h>
